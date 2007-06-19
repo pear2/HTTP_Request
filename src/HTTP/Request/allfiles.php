01a6a93652b54bd7a2b9a2197345911f0c2d1efc
David Coallier <davidc@php.net>
2007-06-19 21:26:14 +0000

    using new version instead of millions of dirname() calls, store it in a variable named by the package append PATH to it.

--- src/HTTP/Request/allfiles.php
+++ src/HTTP/Request/allfiles.php
@@ -4,12 +4,16 @@ if (!class_exists('PEAR2_HTTP_Request_Exception')) {
     class PEAR2_HTTP_Request_Exception extends Exception {}
 }
 
-require_once dirname(__FILE__) . '/HTTP/Request.php';
-require_once dirname(__FILE__) . '/HTTP/Request/Exception.php';
-require_once dirname(__FILE__) . '/HTTP/Request/Response.php';
-require_once dirname(__FILE__) . '/HTTP/Request/Adapter.php';
-require_once dirname(__FILE__) . '/HTTP/Request/Adapter/Phpstream.php';
-require_once dirname(__FILE__) . '/HTTP/Request/Adapter/Curl.php';
+if (!defined('PEAR2_HTTP_Request_PATH')) {
+    define('PEAR2_HTTP_Request_PATH', dirname(__FILE__));
+}
+
+require PEAR2_HTTP_Request_PATH . '/HTTP/Request.php';
+require PEAR2_HTTP_Request_PATH . '/HTTP/Request/Exception.php';
+require PEAR2_HTTP_Request_PATH . '/HTTP/Request/Response.php';
+require PEAR2_HTTP_Request_PATH . '/HTTP/Request/Adapter.php';
+require PEAR2_HTTP_Request_PATH . '/HTTP/Request/Adapter/Phpstream.php';
+require PEAR2_HTTP_Request_PATH . '/HTTP/Request/Adapter/Curl.php';
 
 // PEAR Deps, PEAR must be in the include path
 require_once 'Net/URL2.php';
