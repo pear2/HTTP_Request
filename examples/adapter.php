23705eea4f5108199e95523c04e24206ae5b4a51
Joshua Eichorn <josh@bluga.net>
2007-09-11 21:33:43 +0000

    an example selecting a specific adapter

---
+++ examples/adapter.php
@@ -0,0 +1,21 @@
+<?php
+//require_once 'PEAR2/HTTP/Request/allfiles.php';
+
+// to run from svn checkout
+require_once '../src/HTTP/Request/allfiles.php';
+
+// make a request using the phpstream adapter
+$request = new PEAR2_HTTP_Request('http://pear.php.net/',new PEAR2_HTTP_Request_Adapter_Phpstream);
+$response = $request->sendRequest();
+
+var_dump($response->code);
+var_dump($response->headers);
+var_dump(strlen($response->body));
+
+// make a request using the phpsocket adapter
+$request = new PEAR2_HTTP_Request('http://pear.php.net/',new PEAR2_HTTP_Request_Adapter_Phpsocket);
+$response = $request->sendRequest();
+
+var_dump($response->code);
+var_dump($response->headers);
+var_dump(strlen($response->body));
