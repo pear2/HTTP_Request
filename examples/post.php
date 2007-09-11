4a4ef4d0261394168c2d1a4a69d1ced9e50f7cc5
Joshua Eichorn <josh@bluga.net>
2007-09-11 17:29:47 +0000

    fix post example

--- examples/post.php
+++ examples/post.php
@@ -1,5 +1,8 @@
 <?php
-require_once 'PEAR2/HTTP/Request/AllFiles.php';
+//require_once 'PEAR2/HTTP/Request/AllFiles.php';
+
+// to run from svn checkout
+require_once '../src/HTTP/Request/allfiles.php';
 
 $request = new PEAR2_HTTP_Request('http://bluga.net/post.php');
 $request->body = array('test1'=>'value1','test2'=>'value2');
