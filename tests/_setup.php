c8f45dfa13af4c59d33791a11a2a69134ec24be6
Joshua Eichorn <josh@bluga.net>
2008-05-19 22:52:13 +0000

    Make tests work out of svn, by using the svn autloader

--- tests/_setup.php
+++ tests/_setup.php
@@ -4,4 +4,8 @@ $testServer = "http://bluga.net/projects/PEAR2_HTTP_Request/test/";
 
 // for tests were including all the HTTP_Request files
 // this is a hack to make tests run from svn for now
-require_once dirname(__FILE__).'/../src/HTTP/Request/allfiles.php';
+
+$autoload = dirname(__FILE__).'/../../sandbox/Loader/src/PEAR2/Loader/PEAR2Svn.php';
+if (file_exists($autoload)) {
+	require_once dirname(__FILE__).'/../../sandbox/Loader/src/PEAR2/Loader/PEAR2Svn.php';
+}
