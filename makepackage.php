cd46ffe50e5604619201d9ca3f7020a48304e363
Joshua Eichorn <josh@bluga.net>
2008-05-20 16:45:59 +0000

    update makepackage.php so it will run from svn and update package

--- makepackage.php
+++ makepackage.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors',true);
-require '/Users/bbieber/pyrus/src/PEAR2/Autoload.php';
+require_once dirname(__FILE__).'/../sandbox/Loader/src/PEAR2/Loader/PEAR2Svn.php';
 
 $a = new PEAR2_Pyrus_Developer_PackageFile_PEAR2SVN(dirname(__FILE__), 'PEAR2_HTTP_Request');
 
