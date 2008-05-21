5f86f9ca345d0687751b114b3912953c16f1f743
Joshua Eichorn <josh@bluga.net>
2008-05-21 21:02:35 +0000

    working makepage from svn checkout

--- makepackage.php
+++ makepackage.php
@@ -1,9 +1,17 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors',true);
-require_once dirname(__FILE__).'/../sandbox/Loader/src/PEAR2/Loader/PEAR2Svn.php';
+require_once dirname(__FILE__).'/../autoload.php';
 
 $a = new PEAR2_Pyrus_Developer_PackageFile_PEAR2SVN(dirname(__FILE__), 'PEAR2_HTTP_Request');
 
-
+$package = new PEAR2_Pyrus_Package('package.xml');
+$outfile = $package->name.'-'.$package->version['release'];
+$a = new PEAR2_Pyrus_Package_Creator(
+	new PEAR2_Pyrus_Developer_Creator_Tar($outfile.'.tar', 'none'),
+	dirname(__FILE__).'/../Exception/src',
+	dirname(__FILE__).'/../Autoload/src',
+	dirname(__FILE__).'/../MultiErrors/src'
+);
+$a->render($package);
 ?>
--- tests/_setup.php
+++ tests/_setup.php
@@ -5,7 +5,7 @@ $testServer = "http://bluga.net/projects/PEAR2_HTTP_Request/test/";
 // for tests were including all the HTTP_Request files
 // this is a hack to make tests run from svn for now
 
-$autoload = dirname(__FILE__).'/../../sandbox/Loader/src/PEAR2/Loader/PEAR2Svn.php';
+$autoload = dirname(__FILE__).'/../../autoload.php';
 if (file_exists($autoload)) {
-	require_once dirname(__FILE__).'/../../sandbox/Loader/src/PEAR2/Loader/PEAR2Svn.php';
+	require_once $autoload;
 }
