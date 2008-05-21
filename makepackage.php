10343fb25e5108833bf526130775fc944e808b03
Joshua Eichorn <josh@bluga.net>
2008-05-21 21:49:58 +0000

    more or less working makepackage

--- makepackage.php
+++ makepackage.php
@@ -9,6 +9,7 @@ $package = new PEAR2_Pyrus_Package('package.xml');
 $outfile = $package->name.'-'.$package->version['release'];
 $a = new PEAR2_Pyrus_Package_Creator(
 	new PEAR2_Pyrus_Developer_Creator_Tar($outfile.'.tar', 'none'),
+	//new PEAR2_Pyrus_Developer_Creator_Phar($outfile.'.tgz', false, Phar::TAR, Phar::GZ),
 	dirname(__FILE__).'/../Exception/src',
 	dirname(__FILE__).'/../Autoload/src',
 	dirname(__FILE__).'/../MultiErrors/src'
