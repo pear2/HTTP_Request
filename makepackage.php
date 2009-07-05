b0d14ac92a8d6976c0c67c2f1e143cc38e87d9bd
Greg Beaver <greg@chiaraquartet.net>
2009-07-05 21:34:58 +0000

    move to new Pyrus namespaced API

--- makepackage.php
+++ makepackage.php
@@ -3,13 +3,13 @@ error_reporting(E_ALL);
 ini_set('display_errors',true);
 require_once dirname(__FILE__).'/../autoload.php';
 
-$a = new PEAR2_Pyrus_Developer_PackageFile_PEAR2SVN(dirname(__FILE__), 'PEAR2_HTTP_Request');
+$a = new pear2\Pyrus\PEAR2SVN(dirname(__FILE__), 'PEAR2_HTTP_Request');
 
-$package = new PEAR2_Pyrus_Package('package.xml');
+$package = new pear2\Pyrus\Package('package.xml');
 $outfile = $package->name.'-'.$package->version['release'];
-$a = new PEAR2_Pyrus_Package_Creator(
-	new PEAR2_Pyrus_Developer_Creator_Tar($outfile.'.tar', 'none'),
-	//new PEAR2_Pyrus_Developer_Creator_Phar($outfile.'.tgz', false, Phar::TAR, Phar::GZ),
+$a = new pear2\Pyrus\Creator(
+	new pear2\Pyrus\Tar($outfile.'.tar', 'none'),
+	//new pear2\Pyrus\Phar($outfile.'.tgz', false, Phar::TAR, Phar::GZ),
 	dirname(__FILE__).'/../Exception/src',
 	dirname(__FILE__).'/../Autoload/src',
 	dirname(__FILE__).'/../MultiErrors/src'
