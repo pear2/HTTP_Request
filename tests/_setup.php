cb9f624f9f390bef6f150a09c380f167b6bcbcf8
Brett Bieber <brett.bieber@gmail.com>
2011-07-31 08:28:30 -0500

    Change server reference to localhost

--- tests/_setup.php
+++ tests/_setup.php
@@ -1,6 +1,6 @@
 <?php
 $testServer = false; // set to the url where testFiles has been copied
-$testServer = "http://ucommbieber.unl.edu/workspace/PEAR2/HTTP_Request/tests/testFiles/";
+$testServer = "http://localhost/workspace/PEAR2_HTTP_Request/tests/testFiles/";
 
 // for tests were including all the HTTP_Request files
 // this is a hack to make tests run from svn for now
