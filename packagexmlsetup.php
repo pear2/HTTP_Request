82044a46e09321ead236a0687ce0db40f70a344f
Brett Bieber <brett.bieber@gmail.com>
2012-02-21 13:18:18 -0600

    Ensure the package_compatible.xml file is the same as package.xml

--- packagexmlsetup.php
+++ packagexmlsetup.php
@@ -1,4 +1,6 @@
 <?php
 $package->dependencies['required']->php = '5.3.0';
+$compatible->dependencies['required']->php = '5.3.0';
 
 unset($package->files['tests/testFiles/2meg.bin']);
+unset($compatible->files['test/pear2.php.net/PEAR2_HTTP_Request/testFiles/testFiles/2meg.bin']);
