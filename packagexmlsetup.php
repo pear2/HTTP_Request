fcf8e087748c454a6f310590f976949e1dbe1415
Brett Bieber <brett.bieber@gmail.com>
2012-02-21 13:12:15 -0600

    Ensure the 2mb bin file isn't distributed

--- packagexmlsetup.php
+++ packagexmlsetup.php
@@ -1,4 +1,4 @@
 <?php
 $package->dependencies['required']->php = '5.3.0';
 
-unset($package->files['test/pear2.php.net/PEAR2_HTTP_Request/testFiles/testFiles/2meg.bin']);
+unset($package->files['tests/testFiles/2meg.bin']);
